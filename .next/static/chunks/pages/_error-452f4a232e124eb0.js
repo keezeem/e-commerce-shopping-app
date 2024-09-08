82313a382ac8e59394d4d65ff2354061e71c9ed2
keezeem <azeemolajide7@gmail.com>
2024-09-08 09:07:14 +0100

    first commit

--- .next/static/chunks/pages/_error-452f4a232e124eb0.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{3973:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(5209)}])}},function(n){n.O(0,[888,179],function(){return n(n.s=3973)}),_N_E=n.O()}]);
